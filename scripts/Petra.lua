574f3c4b51009b3be5a39ea16c5f203bfecba6ff
IoriBranford <ioribranford@gmail.com>
2021-06-28 16:25:45 -0700

    Set Petra jump sprite when rising.

--- scripts/Petra.lua
+++ scripts/Petra.lua
@@ -18,17 +18,19 @@ function Petra:startPlay(velx)
     self.think = Petra.thinkPlay
 end
 
-function Petra:collisionFilter(otherid)
+local function collisionFilter(id, otherid)
     return "cross"
 end
 
 function Petra:thinkPlay()
     self.vely = self.vely + self.gravity
-    if self.vely > 0 then
+    if self.vely < 0 then
+        self.sprite:setTile(self.tileset.jump)
+    else
         self.sprite:setTile(self.tileset.fly)
     end
 
-    Unit.move(self, 0, self.vely/2, Petra.collisionFilter)
+    Unit.move(self, 0, self.vely/2, collisionFilter)
 end
 
 function Petra:thinkGameOver()
